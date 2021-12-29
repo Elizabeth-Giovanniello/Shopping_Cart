68993eb7406e547ba67dad6a6c4c0907e2ae6894
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2021-12-28 23:26:45 -0500

    Added comments and adjusted price calculation to limit price to two decimal points.

--- customer.py
+++ customer.py
@@ -4,11 +4,11 @@ class Customer:
     def __init__(self, name):
         self.name = name
         self.customers_cart = ShoppingCart()
-        self.customers_items = self.customers_cart.products
+        self.customers_items = self.customers_cart.products   #created this variable because previously the review_cart method was unable to iterate 
     
     def add_to_customers_cart(self, added_product):
         self.customers_cart.add_to_cart(added_product)
 
     def review_cart(self):
         for item in self.customers_items:
-            print(item.name)
+            print(item.name)     #added .name because previously it was printing every attribute of the item
--- shopping_cart.py
+++ shopping_cart.py
@@ -1,19 +1,17 @@
-#from product import Product
-
 class ShoppingCart:
     def __init__(self):
         self.products = []
     
     def calculate_total_price(self):
-        self.total_price = 0
+        self.total_price = 0   #chose to pass the values in as floats. But could also pass them in as strings and add a step here to convert them to floats so we could find their sum
         for item in self.products:
             self.total_price += item.price
-        return self.total_price
+        return round(self.total_price, 2) #for some reason it was returning a super long decimal, so I added a round function to limit it to 2 decimal points
     
     def add_to_cart(self, new_product):
         self.products.append(new_product)
 
     def empty_cart(self):
-        self.products = []
+        self.products = []  #resets it back to how it was in the beginning, empty but prepared to accept a list (so a customer's cart could be empty but still usable)
 
     
